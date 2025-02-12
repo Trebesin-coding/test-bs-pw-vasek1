e2f27daafeb36cedf0741f644a0c70a83e88cbb2
vasek1 <cistecky121@trebesin.cz>
2025-02-12 08:41:38 +0100

    nahrany test

--- polevka.py
+++ polevka.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
-import requests
+import requests,json
 
 
 def main():
 
+     url ="https://js-trebesin.github.io/bsoup-exam/"
+     response = requests.get(url)
 
-    # response = requests.get(url)
-
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
-
+     soup = BeautifulSoup(response.content, "html.parser")
+     list =  []
+     suroviny = soup.find_all(class_ ="stuff")
+     for p in suroviny:
+          list.append(p.text)
+     jidlo = list[0],list[1],list[2],list[3]         
+     with open("ingredience.json", mode ="w") as soubor:
+          json.dump(jidlo,soubor,indent= 2, ensure_ascii= False)
 
 if __name__ == "__main__":
     main()
--- secret.py
+++ secret.py
@@ -1,21 +1,28 @@
 from playwright.sync_api import sync_playwright
-import os
-
 
 
 
 def main():
 
-    # with sync_playwright() as p:
+     with sync_playwright() as p:
+         login = "Jarmil"
+         password = "Admin123"
 
-        # browser = p.chromium.launch(headless=False)
+         browser = p.chromium.launch(headless=False)
+         page = browser.new_page()
 
-        # !!!
-        # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
-        # !!!
+         page.goto("https://js-trebesin.github.io/playwright-exam/")
+         page.fill('input[id="login"]',login)
+         page.fill('input[id="pass"]',password)
+         page.click('button[class="login-btn"]')
+         
+         jarmil = page.locator('div[class="psst"]').text_content()
 
+         print(jarmil)
 
-        # browser.close()
+         input("vypni")
+
+         browser.close()
     
 
 if __name__ == "__main__":
